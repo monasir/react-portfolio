f097dea1b6d9cd2b9bd9077a63907747fbbcacf9
monasir <mohd.204@live.com>
2018-10-09 14:55:11 +0300

    testing branch creation

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     const anchs = [...document.querySelectorAll('.links')];
     
     const anch = [...anchs];
-    console.log(anchs);
+  
 
     
     this.setState({
